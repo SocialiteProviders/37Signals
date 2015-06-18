40d6621a627890902c51c4b129ce04b968e47707
DraperStudio <hello@draperstud.io>
2015-06-18 14:43:30 +0200

    Require PHP 5.5 and Manager 1.0 (Guzzle 6)

--- src/Provider.php
+++ src/Provider.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace SocialiteProviders\ThirtySevenSignals;
 
 use Laravel\Socialite\Two\AbstractProvider;
@@ -37,7 +38,7 @@ class Provider extends AbstractProvider implements ProviderInterface
             ],
         ]);
 
-        return json_decode($response->getBody(), true)['identity'];
+        return json_decode($response->getBody()->getContents(), true)['identity'];
     }
 
     /**
--- src/ThirtySevenSignalsExtendSocialite.php
+++ src/ThirtySevenSignalsExtendSocialite.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace SocialiteProviders\ThirtySevenSignals;
 
 use SocialiteProviders\Manager\SocialiteWasCalled;
